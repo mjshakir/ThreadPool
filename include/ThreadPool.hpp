e5d89bcc84eb9a4c653f0d57318425c04da316e7
mjshakir <maslove23@gmail.com>
2024-03-16 17:02:53 -0700

    Add: enum for the thread mode

--- include/ThreadPool.hpp
+++ include/ThreadPool.hpp
@@ -23,13 +23,34 @@
 namespace ThreadPool{
     //--------------------------------------------------------------
     /**
+     * @enum ThreadMode
+     * @brief Enum class to specify threading mode for ThreadPool.
+     *
+     * This enum class is used to define the threading mode for the ThreadPool class. It supports two modes:
+     * STANDARD and PRIORITY. STANDARD mode is used for regular operation, while PRIORITY mode allows for
+     * priority-based task execution within the ThreadPool.
+     *
+     * @param ThreadMode::STANDARD
+     * STANDARD mode indicates that the ThreadPool will process tasks in a first-come, first-served manner,
+     * without considering task priority. This is the default mode.
+     *
+     * @param ThreadMode::PRIORITY
+     * PRIORITY mode indicates that the ThreadPool will process tasks based on their priority, allowing
+     * higher priority tasks to be executed before lower priority ones.
+     */
+    enum class ThreadMode : bool {
+        STANDARD = false,
+        PRIORITY = true
+    }; // end enum class ThreadMode
+    //--------------------------------------------------------------
+    /**
      * @class ThreadPool
      * @brief A thread pool implementation that manages worker threads.
      *
      * The ThreadPool class handles the creation, management, and destruction of threads. 
      * It also provides functionality to queue tasks for execution by the worker threads.
      */
-    template <bool use_priority_queue = false>
+    template <ThreadMode use_priority_queue = ThreadMode::STANDARD>
     class ThreadPool {
         //-------------------------------------------------------------
         protected:
