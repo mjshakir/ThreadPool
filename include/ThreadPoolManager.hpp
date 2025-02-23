35c399813ee241c01f337518b2028c7f695fe669
mjshakir <maslove23@gmail.com>
2025-02-23 01:25:44 -0800

    Add: constexpr functions to get the mode and adtoptive size

--- include/ThreadPoolManager.hpp
+++ include/ThreadPoolManager.hpp
@@ -4,6 +4,7 @@
 //--------------------------------------------------------------
 #include <thread>
 #include <cstddef>
+#include <cstdbool>
 //--------------------------------------------------------------
 // User Defined library
 //--------------------------------------------------------------
@@ -89,12 +90,95 @@ namespace ThreadPool {
             ThreadPool<C_THREADMODE, C_ADOPTIVE_TICK>& get_thread_pool(void) {
                 return m_thread_pool;
             }// end ThreadPool<ThreadPoolManager::C_THREADMODE, ThreadPoolManager::C_ADOPTIVE_TICK>& get_thread_pool(void)
+            //--------------------------
+            /**
+             * @brief Get the ThreadPool Mode
+             * 
+             * @return ThreadMode Enum value representing the ThreadPool mode
+             * 
+             * @example
+             * 
+             * @code
+             * 
+             * #include <iostream>
+             * #include "ThreadPoolManager.hpp"
+             * #inlcude "ThreadMode.hpp"
+             * 
+             * int main() {
+             *      // Get the instance of the ThreadPoolManager
+             *      // auto& manager = ThreadPool::ThreadPoolManager::get_instance();
+             * 
+             *      std::cout << "ThreadPool Mode: " << ThreadPool::ThreadMode_name(ThreadPool::ThreadPoolManager::mode()) << std::endl;
+             * 
+             * return 0;
+             * }
+             * 
+             * @endcode
+             */
+            static constexpr ThreadMode mode(void) {
+                return C_THREADMODE;
+            }// end constexpr ThreadMode thread_mode(void)
+            //--------------------------
+            /**
+             * @brief Get adaptive tick size
+             * 
+             * @return size_t The adaptive tick size
+             * 
+             * @example
+             * 
+             * @code
+             * 
+             * #include <iostream>
+             * #include "ThreadPoolManager.hpp"
+             * 
+             * int main() {
+             *      // Get the instance of the ThreadPoolManager
+             *      auto& manager = ThreadPool::ThreadPoolManager::get_instance();
+             * 
+             *      std::cout << "ThreadPool adoptive_tick: " << manager.number_threads() << std::endl;
+             * 
+             * return 0;
+             * }
+             * 
+             * @endcode
+             */
+            static constexpr size_t adoptive_tick(void) {
+                return C_ADOPTIVE_TICK;
+            }// end constexpr size_t adoptive_tick(void)
+            //--------------------------
+            /**
+             * @brief Get if the ThreadPool is adoptive
+             * 
+             * @return bool True if the ThreadPool is adoptive, false otherwise
+             * 
+             * @example
+             * 
+             * @code
+             * 
+             * #include <iostream>
+             * #include "ThreadPoolManager.hpp"
+             * 
+             * int main() {
+             *     // Get the instance of the ThreadPoolManager
+             *     // auto& manager = ThreadPool::ThreadPoolManager::get_instance();
+             * 
+             *    std::cout << "ThreadPool adoptive: " << std::boolalpha << ThreadPool::ThreadPoolManager::adoptive() << std::endl;
+             * 
+             * return 0;
+             * }
+             * 
+             * @endcode
+             */
+            static constexpr bool adoptive(void) {
+                return 0 < C_ADOPTIVE_TICK;
+            }// end constexpr bool adoptive(void)
             //--------------------------------------------------------------
         private:
             //--------------------------------------------------------------
+            ThreadPoolManager(void)                                 = delete;
+            //--------------------------
             explicit ThreadPoolManager(const size_t& number_threads);
             //--------------------------
-            ThreadPoolManager(void)                                 = delete;
             ~ThreadPoolManager(void)                                = default;
             //--------------------------
             // Delete move and copy constructor and assignment operator to ensure singleton properties
